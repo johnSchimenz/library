c931f0015afcc3b414a641ce4834f1a82d480155
John Schimenz <john.schimenz@gmail.com>
2023-08-23 09:58:44 -0500

    Use addBookToLibrary function to push 3 books to library

--- main.js
+++ main.js
@@ -7,8 +7,16 @@ function Book(author, title, pages, read) {
     this.read = read;
 }
 
-function addBookToLibrary() {
-
+function addBookToLibrary(book) {
+    myLibrary.push(book);
 }
+
+const book1 = new Book("Smith, Joe", "I Like Apples", 52, false);
+const book2 = new Book("Terry, Phil", "Peanut Butter Jelly Time", 93, false);
+const book3 = new Book("Zendaya, Betty", "Spiderman Is Awesome", 37, false);
+
+addBookToLibrary(book1);
+addBookToLibrary(book2);
+addBookToLibrary(book3);
 
 console.log(myLibrary);
